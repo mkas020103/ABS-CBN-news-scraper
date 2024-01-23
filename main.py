590c8956bb215d3fd77a78d3df57387abec7d90f
BJCul <92005368+BJCul@users.noreply.github.com>
2024-01-23 22:55:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 '''
 January 23, 2024 7pm
 '''
-
+from flask import Flask, request, render_template, redirect, url_for, jsonify
 from cleaner import clean
 from scraper import scrape
 import pandas as pd
@@ -18,5 +18,36 @@ def main():
     y = clean(y_data)
     y.save('cleaned.csv')
 
+'''
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/scrape', methods = ['POST'])
+def scraper():
+    if request.methods == 'POST':
+        sites = request.form.get('sites')
+        scraper = scrape(sites)
+        return 
+
+@app.route('/cleaner')
+def cleaner():
+    return render_template('cleaner.html')
+
+@app.route('/extract')
+def extract():
+    return render_template('extract.html')
+
+@app.route('/sub')
+def substitute():
+    return render_template('sub.html')
+ 
+'''
+
+
 if __name__ == '__main__':
     main()
+
