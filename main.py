0af003a6c7a46803c9bc887bbc1bdf718188d913
BJCul <92005368+BJCul@users.noreply.github.com>
2024-01-26 20:13:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,26 @@ import pandas as pd
 import time
 import copy
 
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+from scraper import scrape
+from cleaner import clean
+
+app = Flask(__name__)
+
+@app.route('/') 
+def home():
+    return render_template('index.html')
+
+@app.route('/scrape', methods=['POST'])
+def scraper():
+    if request.method == 'POST':
+        sites = request.form.get('site')  # get the csv or link from the form in index.html 
+        print(type(sites))
+        news_scraper = scrape(sites)  # scrape the news using the scrape class
+        scraped_data = news_scraper.df.to_dict(orient='records')  # dataframe to dictionary
+        return jsonify(scraped_data)  # return the scraped_data as JSON
+
+'''
 def main():
     # scrape dataframe 
     x = scrape('sites.csv') #change into a csv file or site link
@@ -20,34 +40,27 @@ def main():
 
 '''
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/scrape', methods = ['POST'])
-def scraper():
-    if request.methods == 'POST':
-        sites = request.form.get('sites')
-        scraper = scrape(sites)
-        return 
-
-@app.route('/cleaner')
-def cleaner():
-    return render_template('cleaner.html')
-
 @app.route('/extract')
 def extract():
     return render_template('extract.html')
 
-@app.route('/sub')
-def substitute():
-    return render_template('sub.html')
- 
-'''
+@app.route('/functions')
+def func():
+    return render_template('functions.html')
 
+@app.route('/tutorial')
+def tutorial():
+    return render_template('tutorial.html')
 
-if __name__ == '__main__':
-    main()
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
+
+if __name__ == '__main__':   
+    app.run(debug=True)
+    
 
